af0a5e2db9a9e36627606bf29eb3c682e33906b3
fxia <fxia@reisystems.com>
2016-04-27 12:26:11 -0400

    add my entrypoint.sh

---
+++ entrypoint.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+
+# wait for all services to start-up
+if [ "$1" = '--wait-for-dependencies' ]; then
+    sh -c "while ! nc -w 1 -z $APP_PORT_80_TCP_ADDR $APP_PORT_80_TCP_PORT; do echo 'Waiting for app...'; sleep 2; done"
+    echo "app is up"
+    sh -c "while ! nc -w 1 -z $PYCSW_PORT_80_TCP_ADDR $PYCSW_PORT_80_TCP_PORT; do echo 'Waiting for pycsw...'; sleep 2; done"
+    echo "pycsw is up"
+fi
+
+# start nginx
+nginx -g "daemon off;"
