6685f0d182e981fdcd90a4645d0c33e9c17c6843
Jason Schulte <jason@mobomo.com>
2018-08-03 18:38:04 -0500

    fix port configuration for app container

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,7 +3,7 @@
 
 # wait for all services to start-up
 if [ "$1" = '--wait-for-dependencies' ]; then
-    sh -c "while ! nc -w 1 -z $APP_PORT_80_TCP_ADDR $APP_PORT_80_TCP_PORT; do echo 'Waiting for app...'; sleep 2; done"
+    sh -c "while ! nc -w 1 -z $APP_PORT_5000_TCP_ADDR $APP_PORT_5000_TCP_PORT; do echo 'Waiting for app...'; sleep 2; done"
     echo "app is up"
     sh -c "while ! nc -w 1 -z $PYCSW_PORT_80_TCP_ADDR $PYCSW_PORT_80_TCP_PORT; do echo 'Waiting for pycsw...'; sleep 2; done"
     echo "pycsw is up"
